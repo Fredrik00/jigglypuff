abaa15b81e392f2a2601eef83d187645c4e3e380
Fredrik <Fredrik@Vatsendvik.com>
2016-04-18 16:27:04 +0200

    pressed effect

--- app/src/main/java/com/example/primer/trening/LoginActivity.java
+++ app/src/main/java/com/example/primer/trening/LoginActivity.java
@@ -1,24 +1,39 @@
 package com.example.primer.trening;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     String string_email = "u";
     String string_password = "p";
+    ImageButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = (ImageButton) findViewById(R.id.sign_in_button);
     }
 
     public void toMain(View view) {
+        login.setColorFilter(Color.argb(100, 0, 0, 0));
+
+        new CountDownTimer(300, 300){
+            public void onFinish(){
+                login.setColorFilter(Color.argb(0, 0, 0, 0));
+            }
+
+            public void onTick(long millisUntilFinished){
+            }
+        }.start();
         Intent intent = new Intent(this, MainActivity.class);
         EditText password = (EditText) findViewById(R.id.password);
         AutoCompleteTextView email = (AutoCompleteTextView) findViewById(R.id.email);
