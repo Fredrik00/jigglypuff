f64840850c58b8837531d85182e9f87bca3da579
Njord <njord.brusten@gmail.com>
2016-04-18 15:50:13 +0200

    email is u, passoword is p

--- app/src/main/java/com/example/primer/trening/LoginActivity.java
+++ app/src/main/java/com/example/primer/trening/LoginActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    String email = "u";
-    String password = "p";
+    String string_email = "u";
+    String string_password = "p";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +21,19 @@ public class LoginActivity extends AppCompatActivity {
     public void toMain(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         EditText password = (EditText) findViewById(R.id.password);
-        EditText email = (EditText) findViewById(R.id.email);
+        AutoCompleteTextView email = (AutoCompleteTextView) findViewById(R.id.email);
 
         String input_email = email.getText().toString();
         String input_password = password.getText().toString();
 
-        if (email.equals(input_email) && password.equals(input_password)){
+        if (string_email.equals(input_email)  && string_password.equals(input_password)){
             startActivity(intent);
         }
+
         else{
             System.out.println("##########################################");
-            System.out.println(email);
-            System.out.println(password);
+            System.out.println(input_email);
+            System.out.println(input_password);
         }
     }
 
