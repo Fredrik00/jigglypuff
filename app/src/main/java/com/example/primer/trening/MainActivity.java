5aec0cc8a823960d63bd15fb52191cb5022ac29b
Fredrik <Fredrik@Vatsendvik.com>
2016-04-19 13:38:47 +0200

    PIES BRAH!

--- app/src/main/java/com/example/primer/trening/MainActivity.java
+++ app/src/main/java/com/example/primer/trening/MainActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         contest = (ImageButton) findViewById(R.id.contest_button);
         update = (ImageButton) findViewById(R.id.update_button);
+
+        PieChart pieChart = (PieChart) findViewById(R.id.chart_daily);
+        PieChart pieChart2 = (PieChart) findViewById(R.id.chart_active);
+        // creating data values
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4000f, 0));
+        entries.add(new Entry(6000f, 1));
+
+        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Done");
+        labels.add("Remaining");
+
+        PieData data = new PieData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        pieChart.setDescription("");
+        pieChart.setData(data);
+        Legend legend = pieChart.getLegend();
+        legend.setEnabled(false);
+
+        pieChart.animateY(3000);
+
+        pieChart2.setDescription("");
+        pieChart2.setData(data);
+        Legend legend2 = pieChart2.getLegend();
+        legend2.setEnabled(false);
+
+        pieChart2.animateY(3000);
     }
 
     public void toContest(View view) {
